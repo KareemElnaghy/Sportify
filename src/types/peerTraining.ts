c2298663cc749b1367c366604bb2c3df3de3b80a
Kareem <kareemelnaghy@aucegypt.edu>
2024-11-06 01:59:28 +0200

    Added Peer Training Type

---
+++ src/types/peerTraining.ts
@@ -0,0 +1,9 @@
+export default class PeerTraining{
+    id: number = 0;
+    trainerEmail: string = "";
+    traineeEmail: string = "";
+    sport: string = "";
+    court: string = "";
+    startTime: Date = new Date('2024-11-05T00:00:00Z');
+	endTime: Date = new Date('2024-11-05T00:00:00Z');
+}
