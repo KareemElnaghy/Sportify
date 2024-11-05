3b44c3c67c9432887581aca377ef5fb8365729fa
arwaabdelkarim <arwaabdelkarim@aucegypt.edu>
2024-11-05 23:44:55 +0200

    Updated Super Admin View

--- src/PMs/Admins/SuperAdmin/SuperAdminPM.ts
+++ src/PMs/Admins/SuperAdmin/SuperAdminPM.ts
@@ -1,3 +1,9 @@
-export interface PMSuperAdmin {}
+export interface PMSuperAdmin {
+    onAddAdmin: (Admin) => void;
+    AdminList: Admin[];
+    SelectedAdmin: Admin[];
+    onDeleteAll: () => void;
+    onDeleteAdmin: (Admin) => void;
+}
 
 export const default_PMSuperAdmin: PMSuperAdmin = {};
