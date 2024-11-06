2649d8e4f9dab902900367f983fe326bb19fe3d4
Kareem <kareemelnaghy@aucegypt.edu>
2024-11-06 03:12:40 +0200

    added api files

--- src/Models/Admins/AdminLoginModel.ts
+++ src/Models/Admins/AdminLoginModel.ts
@@ -1,13 +1,19 @@
 import { PMAdminLogin } from "@/PMs/Admins/AdminLogin/AdminLoginPM";
 
 export interface AdminLoginModel {
-	setup: () => Promise<void>;
+  setup: () => Promise<void>;
+  onLogin: () => void;
 }
 
-export function getAdminLoginModel(pm: PMAdminLogin): AdminLoginModel {
-	const model: AdminLoginModel = {
-		setup: async () => {},
-	};
+export function getAdminLoginModel(pm: PMAdminLogin, router: any): AdminLoginModel {
+  const model: AdminLoginModel = {
+    setup: async () => {
+      pm.onLOGIN = model.onLogin;
+    },
+    onLogin: () => {
+      router.push("/admin/superadmin");
+    },
+  };
 
-	return model;
+  return model;
 }
--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -8,11 +8,13 @@ import {
 } from "@/PMs/Admins/AdminLogin/AdminLoginPM";
 import { getAdminLoginModel } from "@/Models/Admins/AdminLoginModel";
 import AdminLoginView from "@/Views/Admins/AdminLogin/AdminLoginView";
+import { useRouter } from "next/navigation";
 
 export default function AdminLoginPage() {
+	const router = useRouter();
 	const pm = useStateObject<PMAdminLogin>(default_PMAdminLogin);
 	const model = useMemo(() => {
-		const model = getAdminLoginModel(pm);
+		const model = getAdminLoginModel(pm, router);
 		model.setup();
 		return model;
 	}, []);
