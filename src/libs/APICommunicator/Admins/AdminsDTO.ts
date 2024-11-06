109516b9113468ec5b1e7efb95144492717bd200
khoulykid <khoulykid@aucegypt.edu>
2024-11-06 05:37:19 +0200

    Added frontend API but not in model

--- src/libs/APICommunicator/Admins/AdminsDTO.ts
+++ src/libs/APICommunicator/Admins/AdminsDTO.ts
@@ -1,17 +1,19 @@
-import Admin from "@/types/Admin";
+import APIResponse from "@/types/APIResponse";
+import AdminLogin from "@/types/Admin";
 
 export interface AdminDTO {
     email: string;
     firstName: string;
 	lastName: string;
     isSuper: boolean;
+    passHash: string;
 }
 
-export const AdminDTOExtractor = (api_response: any): AdminDTO => {
-    return {
-        email: api_response.email,
-        firstName: api_response.firstName,
-        lastName: api_response.lastName,
-        isSuper: api_response.isSuper,
-    };
-}
+export const AdminDTOExtractor = (api_response: APIResponse<any>): AdminLogin[] => {
+    return api_response.result as AdminLogin[];
+
+};
+
+export const AdminDTOTransformer = (dto_object: AdminDTO[]): AdminLogin[] => {
+    return dto_object;
+};
