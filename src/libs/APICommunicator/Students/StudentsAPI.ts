0b4fdb2386b6352cfcfbeac4214e456c7dc288ae
khoulykid <khoulykid@aucegypt.edu>
2024-11-19 18:09:13 +0200

    Started working on students API

--- src/libs/APICommunicator/Students/StudentsAPI.ts
+++ src/libs/APICommunicator/Students/StudentsAPI.ts
@@ -1,25 +1,30 @@
+import Student from "@/types/Student";
 import { APIConnector } from "../APIConnector";
 import {
-	StudentsListData,
-	StudentsListDTOExtractor,
-	StudentsListDTOTransformer,
+  StudentsListData,
+  StudentsListDTOExtractor,
+  StudentsListDTOTransformer,
 } from "./StudentsDTO";
 
 interface getStudentsData {
-	page: number;
-	recordsPerPage: number;
-	searchQuery: string;
+  page: number;
+  recordsPerPage: number;
+  searchQuery: string;
 }
 
 export async function getStudents(
-	data: getStudentsData
+  data: getStudentsData
 ): Promise<StudentsListData> {
-	const response = await APIConnector.get("/api/profiles/list", {
-		page: `${data.page}`,
-		recordsPerPage: `${data.recordsPerPage}`,
-		...(data.searchQuery != "" && { searchQuery: data.searchQuery }),
-	});
-	const responseDTO = StudentsListDTOExtractor(response);
-	const result = StudentsListDTOTransformer(responseDTO);
-	return result;
+  const response = await APIConnector.get("/api/profiles/list", {
+    page: `${data.page}`,
+    recordsPerPage: `${data.recordsPerPage}`,
+    ...(data.searchQuery != "" && { searchQuery: data.searchQuery }),
+  });
+  const responseDTO = StudentsListDTOExtractor(response);
+  const result = StudentsListDTOTransformer(responseDTO);
+  return result;
 }
+
+interface getStudentsEmails
+
+export async function getStudentItems(data: getStudentsItemData): Promise<>;
